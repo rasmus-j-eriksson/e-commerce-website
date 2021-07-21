369c3149a9a7910fe2fa7be331c5b3b91597756e
Rasmus <rasmus.eriksson@fast2.se>
2021-07-21 18:24:55 +0200

    added sign in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { Route, Link, Switch } from 'react-router-dom';
 import './App.css'
 
@@ -39,15 +40,16 @@ const WomensPage = (props) => {
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop/hats' component={HatsPage} />
         <Route exact path='/shop/jackets' component={JacketsPage} />
         <Route exact path='/shop/mens' component={MensPage} />
         <Route exact path='/shop/womens' component={WomensPage} />
-        <Route exact path='/shop/sneakers' component={SneakersPage } />
-        <Route exact path='/shop/' component={ShopPage } />
+        <Route exact path='/shop/sneakers' component={SneakersPage} />
+        <Route exact path='/shop/' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
