c18ac3b244f26ba8413c2fefd099872201671ea4
Rasmus <rasmus.eriksson001@gmail.com>
2021-07-22 13:45:58 +0200

    added login and domain information for rasmus.page

--- src/App.js
+++ src/App.js
@@ -7,47 +7,12 @@ import { Route, Link, Switch } from 'react-router-dom';
 import './App.css'
 
 
-const HatsPage = (props) => {
-  return <div>
-    <h1>HATS PAGE</h1>
-  </div>
-};
-
-const JacketsPage = (props) => {
-  return <div>
-    <h1>JACKETS PAGE</h1>
-  </div>
-};
-
-const SneakersPage = (props) => {
-  return <div>
-    <h1>SNEAKERS PAGE</h1>
-  </div>
-};
-
-const MensPage = (props) => {
-  return <div>
-    <h1>MENS PAGE</h1>
-  </div>
-};
-
-const WomensPage = (props) => {
-  return <div>
-    <h1>WOMENS PAGE</h1>
-  </div>
-};
-
 function App() {
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
-        <Route exact path='/shop/jackets' component={JacketsPage} />
-        <Route exact path='/shop/mens' component={MensPage} />
-        <Route exact path='/shop/womens' component={WomensPage} />
-        <Route exact path='/shop/sneakers' component={SneakersPage} />
         <Route exact path='/shop/' component={ShopPage} />
         <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
