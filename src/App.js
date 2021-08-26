c8c13e3f45c0260580ad67b2889ad6b08ab4878c
Rasmus <rasmus.eriksson001@gmail.com>
2021-08-26 18:51:33 +0200

    made userActionTypes constant

--- src/App.js
+++ src/App.js
@@ -46,11 +46,11 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop/' component={ShopPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? (
-          <Redirect to='/' />
-          ) : (
-          <SignInAndSignUpPage/>
-          )} />
+          <Route exact path='/signin' render={() => this.props.currentUser ? 
+          (<Redirect to='/' />) 
+          : 
+          (<SignInAndSignUpPage/>)} 
+          />
         </Switch>
       </div>
     );
