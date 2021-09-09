eb4fbd850634e4022a31e0bca55a3270b5694fef
Rasmus <rasmus.eriksson001@gmail.com>
2021-09-09 18:16:44 +0200

    updated cart functionality

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -9,12 +9,12 @@ const CollectionPreview = ({ title, items, routeName, history, match }) => (
         <h1 className='title'> {title.toUpperCase()} </h1>
         <div className='preview' onClick={() => history.push(`${match.url}${routeName}`)}>
             {
-            items.filter((item, i) => i < 4)
-                .map(({id, ...otherItemProps}) => (
-                    
-                    <CollectionItem key={id} {...otherItemProps}/>
-                
-                ))
+                items.filter((item, i) => i < 4)
+                    .map((item) => (
+
+                        <CollectionItem key={item.id} item={item} />
+
+                    ))
             }
         </div>
     </div>
